95a007581fc2087da0524993cf59dc1d8149f6ad
LouisBerBiq <louis.beriotbiquet@student.hepl.be>
2021-04-16 03:31:39 +0200

    Initial Student Commit

---
+++ views/user.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>
+	  Pierre
+	</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+  <body>
+  	<h1>Pierre Pomier</h1>
+  	<p>pompipi@gmail.com</p>
+	<div>
+		<a href="index.php">Go back to list</a>
+	</div>
+  </body>
+</html>
---
+++ views/users.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>
+	  all users
+	</title>
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+  </head>
+  <body>
+	<ul>
+		<li><a href="?id=1">user 1</a></li>
+		<li><a href="?id=2">user 2</a></li>
+		<li><a href="?id=3">user 3</a></li>
+	</ul>
+  </body>
+</html>
